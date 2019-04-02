2214683d10aebe2119ba4de33cad0e7327dba235
Dan O'Leary <impulsecoupled@gmail.com>
2019-04-01 21:21:54 -0700

    Initial Commit

---
+++ Day59Consolodation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day59Consolodation
+//
+//  Created by Daniel O'Leary on 4/1/19.
+//  Copyright © 2019 Impulse Coupled Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
